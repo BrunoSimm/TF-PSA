bd75bec17c2a66516b2060f099ce9ce86a471965
Rafael Dias Coll Oliveira <Rafael_Oliveira3@Dellteam.com>
2022-06-15 15:46:09 -0300

    Tables alignment and labels

--- Entidades/Modelos/HorarioGrade.cs
+++ Entidades/Modelos/HorarioGrade.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Entidades.Modelos
 {
     public class HorarioGrade
     {
         public long Id { get; set; }
+
+        [Display(Name = "Período")]
         public string Horario { get; set; } //6NP 5LM ...
         public virtual IEnumerable<Turma> Turmas { get; set; }
+        
+        [Display(Name = "Horário Inicial")] 
         public string HorarioInicial { get; set; }
+        
+        [Display(Name = "Horário Final")] 
         public string HorarioFinal { get; set; }
         public int DiaDaSemana => Convert.ToInt32(Horario[0]);
         public string Periodo => Horario.Substring(Horario.Length - 2);
